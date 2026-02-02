cf17edda85da7b3301114e6528bdd0d51d460bb1
Matteo Fulvi <mattyfulvi01@gmail.com>
2026-02-02 15:57:59 +0100

    inizio test di attacco

---
+++ test/Test2.java
@@ -0,0 +1,12 @@
+package test;
+
+import game.Battlefield;
+import game.MyException;
+import game.Player;
+import piece.generals.*;
+
+public class Test2 {
+    public static void main(String[] args) {
+        
+    }
+}
