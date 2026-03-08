2275310243a29b8a101f0bf2709112d17ede3dbe
Matteo Fulvi <mattyfulvi01@gmail.com>
2026-03-08 11:28:39 +0100

    Commenti di Troops

--- piece/Troops.java
+++ piece/Troops.java
@@ -4,18 +4,19 @@ import game.Battlefield;
 import game.MyException;
 
 public abstract class Troops implements Units{
-    private final String name;
-    private int hp;
-    private final int atk;
-    private final int range;
-    private final int mov;
-    private int stamina;
-    private final int maxStamina;
-    private boolean hasAttacked;
-    private int x;
-    private int y;
-    private final boolean faction;
+    private final String name;               /* Nome unita */
+    private int hp;                          /* Punti ferita */
+    private final int atk;                   /* Valore di attacco */
+    private final int range;                 /* Distanza attacco */
+    private final int mov;                   /* Distanza di movimento dell'unita */
+    private int stamina;                     /* Numero di mosse rimanenti di questa unita */
+    private final int maxStamina;            /* Numero massimo di movimenti per turno di questa unita */
+    private boolean hasAttacked;             /* Flag che mostra se questa unita ha attaccato durante il turno */
+    private int x;                           /* Coordinata X */
+    private int y;                           /* Coordinata Y */
+    private final boolean faction;           /* Flag che determina la fazione e quindi se si tratta di Player1 o Player2 */
     
+    /* Costruttore di Troops */
     public Troops (String name, int hp, int atk, int range, int mov, int maxStamina, boolean faction) {
         this.name = name;
         this.hp = hp;
@@ -28,84 +29,73 @@ public abstract class Troops implements Units{
         this.faction = faction;
     }
     
+    /* Funzione che restituisce vero se l'unita e' ancora in vita */
     @Override
     public boolean isAlive() {
         return hp > 0;
     }
     
+    /* Funzione che restituisce vero se l'unita appartiene a Player1 */
     @Override
     public boolean isHost() {
 		return faction;
 	}
+
     //metodi getter
-    
     public String getName() {
         return this.name;
     }
-    
     public int getHP() {
         return this.hp;
-    }
-    
+    } 
     public int getAtk() {
         return this.atk;
     }
-    
     public int getRange() {
         return this.range;
     }
-    
     public int getMov() {
         return this.mov;
     }
-    
     public int getStamina() {
         return this.stamina;
     }
-    
     public int getMaxStamina() {
         return this.maxStamina;
     }
-    
     public boolean getHasAttacked() {
         return this.hasAttacked;
     }
-    
     public int getX() {
         return this.x;
     }
-
     public int getY() {
         return this.y;
     }
-    
     public boolean getFaction() {
         return this.faction;
     }
     
     //metodi setter
-    
     public void setHP(int hp) {
         this.hp = hp;
     }
-    
     public void setStamina(int stamina) {
         this.stamina = stamina;
     }
-    
     public void setHasAttacked(boolean hasAttacked) {
         this.hasAttacked = hasAttacked;
     }
-    
     public void setX(int x) {
         this.x = x;
     }
-
     public void setY(int y) {
         this.y = y;
     }
     
     //funzione che definisce l'attacco
+    /* Riceve in entrata le coordinate del bersaglio ed il campo di battaglia.
+     * Se l'attacco non ha successo genera una eccezione */
     @Override
     public void attack(int targetX, int targetY, Battlefield field) throws MyException{
         Units targetUnit = field.getUnit(targetX, targetY);
@@ -151,6 +141,8 @@ public abstract class Troops implements Units{
     }
     
     //funzione di movimento delle truppe
+    /* Riceve in entrata il campo di battaglia e la direzione del movimento.
+     * Se il movimento fallisce genera una eccezione */
     @Override
     public void move(Battlefield field, int direction) throws MyException{
         int currentX = getX();
@@ -218,12 +210,13 @@ public abstract class Troops implements Units{
         }
     }
     
+    /* Questa funzione fa subire i danni all'unita */
     @Override
     public void attacked(int atk) {
 		setHP(getHP()-atk);
 	}
     
-    //funzione che calcola la distanza tra due punti
+    /* Funzione che calcola la distanza tra due punti */
     private int calculateDistance(int x1, int y1, int x2, int y2) {
         /* visto che il movimento diagonale vale come 1 casella,
         la distanza fra due punti è la differenza maggiore fra le X e le Y */
@@ -234,36 +227,39 @@ public abstract class Troops implements Units{
         return distance;
     }
 
-    //funzione che calcola se l'obiettivo selezionato è a portata dell'attaccante
+    /* Funzione che calcola se l'obiettivo selezionato è a portata dell'attaccante */
     private boolean isInRange(int targetX, int targetY) {
         int distance = calculateDistance(getX(), getY(), targetX, targetY);
         return distance <= getRange();
     }
 
+    /* Funzione che conferma se l'azione di movimento e' accettabile */
     private boolean isValidMove(int newX, int newY, Battlefield field) throws MyException {
-            if (newX < 0 || newX > field.getX() - 1 || newY < 0 || newY > field.getY() - 1) {
-                //fuori dal campo
-                throw new MyException("fuori campo di battaglia");
-                
-            } else if (field.getUnit(newX, newY) != null) {
-                //casella occupata
-                throw new MyException("cella occupata");
-    
-            } else if (getStamina() == 0) {
-                //stamina esaurita
-                throw new MyException("stamina esaurita");
+        if (newX < 0 || newX > field.getX() - 1 || newY < 0 || newY > field.getY() - 1) {
+            //fuori dal campo
+            throw new MyException("fuori campo di battaglia");
+            
+        } else if (field.getUnit(newX, newY) != null) {
+            //casella occupata
+            throw new MyException("cella occupata");
 
-            } else {
-                //tutto ok
-                return true;
-            }
+        } else if (getStamina() == 0) {
+            //stamina esaurita
+            throw new MyException("stamina esaurita");
+
+        } else {
+            //tutto ok
+            return true;
         }
-        
+    }
+     
+    /* Funzione che ricarica la stamina */
     @Override
     public void recharge(){
         setStamina(maxStamina);
     }
 
+    /* Usato per test */
     @Override
     public String toString(){
 		String s="";
